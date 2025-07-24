fcef648227788fc47a69cc5d848c704efc3dbce8
J Mauricio G Salaices <51425235+jmgsalaices@users.noreply.github.com>
2025-07-24 03:29:08 -0600

    Add Application Model
    Applications List View
          Displays a list of submitted requests (applications).
          Fields shown:
          id
          date
          type (request / offer / complaint)
          status (submitted / completed)

---
+++ AuthApi/Models/Application.cs
@@ -0,0 +1,8 @@
+﻿public class Application
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; } = DateTime.UtcNow;
+    public string Type { get; set; } = "request"; // request / offer / complaint
+    public string Message { get; set; }
+    public string Status { get; set; } = "submitted"; // submitted / completed
+}
